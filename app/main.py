0f2fc95cd2e9eea927da25dae0b6e503a244f11a
lyutov_e <eliutov22@gmail.com>
2025-11-19 11:45:17 +0500

    CREATE database

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 
+from .database import engine, Base
+from .models import User, Transaction, Category, Group
+
 
 app = FastAPI(title="Spending Tracker API")
+
+@app.on_event("startup")
+async def startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+@app.on_event("shutdown")
+async def shutdown():
+    await engine.dispose()
 
 @app.get("/")
 def root():
---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from .user import User
+from .transaction import Transaction
+from .category import Category
+from .group import Group
+from .base import Base
