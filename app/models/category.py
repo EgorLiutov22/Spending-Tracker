763beb3f50b53aec321e4b75f8b218abaf7b8635
NinelNina <ninkr@list.ru>
2025-12-19 00:52:30 +0300

    Removed user relationship from category model.

--- app/models/category.py
+++ app/models/category.py
@@ -10,16 +10,6 @@ class Category(Base):
     name = Column(String(100), nullable=False, unique=True)
     description = Column(Text, nullable=True)
 
-    # Добавляем связь с пользователем
-    user_id = Column(
-        Integer, 
-        ForeignKey('users.id', ondelete='CASCADE'), 
-        nullable=False,
-        comment='ID пользователя, которому принадлежит категория'
-    )
-    
-    # Связи с другими моделями
-    user = relationship("User", back_populates="categories")
     transactions = relationship(
         "Transaction", 
         back_populates="category",
@@ -27,4 +17,4 @@ class Category(Base):
     )
 
     def __repr__(self):
-        return f"<Category(id={self.id}, name='{self.name}', user_id={self.user_id}))>"
+        return f"<Category(id={self.id}, name='{self.name}')>"
